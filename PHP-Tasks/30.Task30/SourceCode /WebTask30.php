21d8b9ce29a641cd6f7444040d2e00a0f15ab104
Shashi <sasiwebdev7@gmail.com>
2021-02-02 21:26:04 +0000

    Uncommented the error code

--- PHP-Tasks/30.Task30/SourceCode /WebTask30.php
+++ PHP-Tasks/30.Task30/SourceCode /WebTask30.php
@@ -1,22 +1,22 @@
-<?php 
+<!-- <?php 
 
-class Person {
-	public $arms;
-	public $legs;
-	// Add a constructor to this class that should receive a parameter $message and assign it to $message property of class
-	public __constructor($arms, $legs) {
-		$this->arms = $arms;
-		$this->legs = $legs;
-	}
-	public function showPerson() {
-		/* When this function is called from an object, Echo Output as follow:
-		 (This person has 2 arms and 2 legs) */
-	}
-}
+// class Person {
+// 	public $arms;
+// 	public $legs; 
+// Add a constructor to this class that should receive a parameter $message and assign it to $message property of class
+// public __constructor($arms, $legs) {
+// $this->arms = $arms;
+// $this->legs = $legs;
+// }
+// public function showPerson() {
+// /* When this function is called from an object, Echo Output as follow:
+// (This person has 2 arms and 2 legs) */
+// }
+// }
 
-// Create Two objects of class Person. 
+// Create Two objects of class Person.
 
-$person = new Person();
-$person->showPerson();
+// $person = new Person();
+// $person->showPerson();
 
 ?>
